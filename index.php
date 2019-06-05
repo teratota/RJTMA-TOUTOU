79714ac6ed719e07120a6ad103e5e25218d92196
AntoineMT <antoinemt@outlook.fr>
2019-06-05 11:08:59 +0200

    Recup

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+header('Location: ./src/index.php');
+exit;
+
+?>
---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    include 'navbar.php';
+    include 'body.php';
+    include 'form.php';
+    include 'foot.php';
+    include 'footer.php';
+
