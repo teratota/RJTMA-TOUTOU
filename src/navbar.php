a6643e78fb098b4c0d082dfd8eea8a5b4eed2354
Hoprikoma <md.del@hotmail.fr>
2019-06-05 15:26:59 +0200

    Navbar

--- src/navbar.php
+++ src/navbar.php
@@ -2,12 +2,15 @@
 
 <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
   <img src="./img/logo.svg" width="60" height="60" alt="">
-  <a class="navbar-brand" href="#">TouTou Garderie</a>
+  <a class="navbar-brand" href="index.php">TouTou Garderie</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Accueil</a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="offre.php">Nos Offres</a>
       </li>
@@ -22,6 +25,9 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="index.php#galerie">Galerie</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="tarif.php">Nos tarifs</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="index.php#contact">Contact</a>
