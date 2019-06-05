15fdba78a4e131c9d545c7c64080a575e06b44e3
Hoprikoma <md.del@hotmail.fr>
2019-06-05 14:40:39 +0200

    Mise a jour

--- src/equipe.php
+++ src/equipe.php
@@ -2,73 +2,71 @@
     include 'header.php';
 ?>
 
-<div class='corps shadow p-3 mb-5 bg-white rounded'>
+<h1>L'équipe</h1>
 
-    <h1>L'équipe</h1>
+<img src="./img/equipe.jpg" />
 
-    <img src="./img/equipe.jpg" />
-    
-    <div class="equipe">
+<div class="equipe">
 
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="./img/pdg.jpg" alt="pdg">
-          <div class="card-body">
-            <h5 class="card-title">Rachel Green</h5>
-            <p class="card-text">Gérante de Garderie Toutou</p>
-            <p class="card-text">Elle est la fondatrice de Garderie Toutou et décide des grandes orientations que prendra Garderie Toutou</p>
-          </div>
-        </div>
-
-
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="./img/masseur.jpg" alt="pdg">
-          <div class="card-body">
-            <h5 class="card-title">Joey Tribiani</h5>
-            <p class="card-text">Masseur professionnel</p>
-            <p class="card-text">Il masseras votre chien pour qu'il puisse être plus détendue</p>
-          </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="./img/equipe4.jpg" alt="pdg">
-          <div class="card-body">
-            <h5 class="card-title">Phoebe Buffay</h5>
-            <p class="card-text">Masseuse</p>
-            <p class="card-text">Notre seconde masseur qui va s'occuper du bien-être de votre chien</p>
-          </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="./img/veterinaire.jpg" alt="pdg">
-          <div class="card-body">
-            <h5 class="card-title">Chandler Bing</h5>
-            <p class="card-text">Vétérinaire</p>
-            <p class="card-text">Il vous donneras des conseils pour le bien-être de votre chien</p>
-          </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="./img/equipe2.jpg" alt="pdg">
-          <div class="card-body">
-            <h5 class="card-title">Ross Geller</h5>
-            <p class="card-text">Responsable communication</p>
-            <p class="card-text">Il s'occupe de répondre à toute vos questions que vous posez</p>
-          </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="./img/equipe3.jpg" alt="pdg">
-          <div class="card-body">
-            <h5 class="card-title">Monica Geller</h5>
-            <p class="card-text">Coach</p>
-            <p class="card-text">Elle s'occupe du service de coaching, elle renouera votre lien avec votre chien</p>
-          </div>
-        </div>
-
-
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="./img/pdg.jpg" alt="pdg">
+    <div class="card-body">
+      <h5 class="card-title">Rachel Green</h5>
+      <p class="card-text">Gérante de Garderie Toutou</p>
+      <p class="card-text">Elle est la fondatrice de Garderie Toutou et décide des grandes orientations que prendra
+        Garderie Toutou</p>
     </div>
+  </div>
+
+
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="./img/masseur.jpg" alt="pdg">
+    <div class="card-body">
+      <h5 class="card-title">Joey Tribiani</h5>
+      <p class="card-text">Masseur professionnel</p>
+      <p class="card-text">Il masseras votre chien pour qu'il puisse être plus détendue</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="./img/equipe4.jpg" alt="pdg">
+    <div class="card-body">
+      <h5 class="card-title">Phoebe Buffay</h5>
+      <p class="card-text">Masseuse</p>
+      <p class="card-text">Notre seconde masseur qui va s'occuper du bien-être de votre chien</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="./img/veterinaire.jpg" alt="pdg">
+    <div class="card-body">
+      <h5 class="card-title">Chandler Bing</h5>
+      <p class="card-text">Vétérinaire</p>
+      <p class="card-text">Il vous donneras des conseils pour le bien-être de votre chien</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="./img/equipe2.jpg" alt="pdg">
+    <div class="card-body">
+      <h5 class="card-title">Ross Geller</h5>
+      <p class="card-text">Responsable communication</p>
+      <p class="card-text">Il s'occupe de répondre à toute vos questions que vous posez</p>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="./img/equipe3.jpg" alt="pdg">
+    <div class="card-body">
+      <h5 class="card-title">Monica Geller</h5>
+      <p class="card-text">Coach</p>
+      <p class="card-text">Elle s'occupe du service de coaching, elle renouera votre lien avec votre chien</p>
+    </div>
+  </div>
+
 
 </div>
+
 
 <?php
     include 'footer.php';
--- src/offre.php
+++ src/offre.php
@@ -2,15 +2,15 @@
     include 'header.php';
 ?>
 
-<div class='corps shadow p-3 mb-5 bg-white rounded'>
 
-    <h1>Nos Offres</h1>
+<h1>Nos Offres</h1>
 
-    <p>Nous vous proposons nos offres découverte, qui vous permettent d'utiliser nos services gratuitement lors de votre première fois dans notre entreprise.</p>
+<p>Nous vous proposons nos offres découverte, qui vous permettent d'utiliser nos services gratuitement lors de votre
+    première fois dans notre entreprise.</p>
 
-    <p>Cela vous permettra d'essayer nos services sans les payers pour vous permettre de découvrir la qualité de nos service.</p>
+<p>Cela vous permettra d'essayer nos services sans les payers pour vous permettre de découvrir la qualité de nos
+    service.</p>
 
-</div>
 
 <?php
     include 'footer.php';
