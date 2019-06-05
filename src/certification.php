b4d47edf01ed53443e7fde8ba6c404efa6c7dded
Lucie <rui.chen1996@gmail.com>
2019-06-05 13:01:53 +0200

    create Page Certification

---
+++ src/certification.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruich
+ * Date: 05/06/2019
+ * Time: 12:21
+ */
+
+include 'header.php';
+include 'navbar.php';
+include 'footer.php';
+
+?>
+
+<div class='corps shadow p-3 mb-5 bg-white rounded'><br><br>
+    <div>
+        <h1>Certification</h1>
+
+        <div style="float:left;width:20%;">
+            <img src="./img/DogBuddy.jpg" class="" alt="dogbuddy" >
+        </div>
+        <div style="float:left;width:25%; margin-left: margin-left: 50%;">
+            <img src="./img/iso9001.png" class="" alt="norme" >
+        </div>
+
+    </div>
+
+</div>
