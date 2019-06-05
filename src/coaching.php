fd4baae002ab4ed2ae2877e9495f5203c8bfef1d
Lucie <rui.chen1996@gmail.com>
2019-06-05 12:13:05 +0200

    Create Page Coaching Bine être

---
+++ src/coaching.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruich
+ * Date: 05/06/2019
+ * Time: 11:04
+ */
+    include 'header.php';
+    include 'navbar.php';
+    include 'footer.php';
+?>
+
+<div class='corps shadow p-3 mb-5 bg-white rounded'>
+    <h1>Coaching Bien-être</h1>
+    <h2> Nous savons que le chien est le meilleur ami de l'Homme.</h2>
+
+    <img src="./img/chien_coaching.jpeg" class="coach_img" alt="chien_coaching" >
+
+
+    <div class="coach1">
+        <h3>Que'est ce que c'est...</h3>
+        <br>
+        <p>Nous pouvons controler en quelques points</p>
+        <ul>
+            <li>Mettez votre coeur en forme</li>
+            <p>La marche, comme tous les exercices aérobiques, renforce les muscles cardiaques,
+                ils peuvent pomper plus de sang dans tout le corps avec un minimum d'effort chaque battement.
+                le travail cardiaque autour de l'horloge,
+                et de réduire leur charge de travail ne modifie pas la quantité de pompe à sang peut réduire le risque de maladies cardio - vasculaires et de ses complications, telles que l' hypertension artérielle ,
+                accident vasculaire cérébral , crise cardiaque & insuffisance cardiaque .</p>
+            <p>
+                Faites des promenade régulire permet de maintenir les foncions cellulaires les plus importantes,
+                et de favoriser les nutriment et l'oxygène nécessairesà  la réparation et des guérison des tissus.
+            </p>
+        </ul>
+    </div>
+
+
+
+
+
+    <div class="coach2">
+        <h3>Ce qu'il faut faire</h3>
+        <ul>
+            <li>Hydrater avec votre chien</li>
+            <p>Laissez-vous hydrater avec votre chien, surtout lorsque vous vous promenez par temps chaud. Essayez de mettre de l'eau pour votre chien, également.</p>
+        </ul>
+    </div>
+
+
+
+    <div class="coach3">
+        <h3>Ce qu'il faut pas faire </h3>
+        <ul>
+            <li>Vérifiez les conditions météorologiques et les itinéraires de promenade</li>
+            <p>Marcher dehors à des températures extrêmes est inconfortable et dangereux pour vous et votre chien. </p>
+            <li>Ne vous exercez pas trop les activités physique</li>
+            <p>la marche est un excellent moyen de rester en forme, mais faire autre chose que de le faire n'est pas bon pour votre corps et votre chien.
+                Si vous vous sentez fatigué ou si votre chien a l'air fatigué, faites une pause. Recherchez un endroit frais.</p>
+        </ul>
+    </div>
+
+</div>
+
+
